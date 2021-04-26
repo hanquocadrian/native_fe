1b78d2a97a4ed3d5acee224e8a599800e542d112
hanquocadrian <hanquocadrian@gmail.com>
2021-04-26 10:19:13 +0700

    Update code About_Admin to Func Component

--- src/Admin/Components/About/About.jsx
+++ src/Admin/Components/About/About.jsx
@@ -1,23 +1,67 @@
-import React, { Component } from 'react';
-import auth from "Admin/auth";
+// import React from 'react';
+// import auth from "Admin/auth";
 
-export default class index extends Component {
-    render() {
-        const sendData = this.props.location.sendData;
-        return (
-            <>
-                <p>About Admin</p>
-                <p>
-                    My name is { sendData.me }, height: { sendData.height }, weight: { sendData.weight }
-                </p>
-                <button onClick={ () => {
-                    auth.logout(() => {
-                        this.props.history.push("/admin/");
-                    });
-                }} >
-                    Logout
-                </button>
-            </>
-        )
-    }
+// export default class index extends Component {
+//     render() {
+//         const sendData = this.props.location.sendData;
+//         return (
+//             <>
+//                 <p>About Admin</p>
+//                 <p>
+//                     My name is { sendData.me }, height: { sendData.height }, weight: { sendData.weight }
+//                 </p>
+//                 <button onClick={ () => {
+//                     auth.logout(() => {
+//                         this.props.history.push("/admin/");
+//                     });
+//                 }} >
+//                     Logout
+//                 </button>
+//             </>
+//         )
+//     }
+// }
+
+import React, { useState, useEffect } from 'react';
+import auth from 'Admin/auth';
+
+function About(props) {
+    
+    const [sendData, setSendData] = useState({
+        myName: "",
+        height: 0,
+        weight: 0
+    });
+    useEffect(() => {
+        function getInfo (props) {
+            setSendData({
+                myName: props.location.sendData.me,
+                height: props.location.sendData.height,
+                weight: props.location.sendData.weight
+            })
+        }
+        getInfo(props);
+        return () => {
+            // setSendData({sendData});
+        }
+    }, [props]);
+    return (
+        <>
+            <h3>About Admin</h3>
+            <p>
+                My name is { sendData.myName }, height: { sendData.height }, weight: { sendData.weight }
+            </p>
+            <button onClick={ () => {
+                auth.logout(() => {
+                    props.history.push("/admin/");
+                });
+            }} >
+                Logout
+            </button>
+        </>
+    )
 }
+
+export default About
+
+
